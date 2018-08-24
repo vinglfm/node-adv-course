bea42d569ba12351aeacb2096005840d041b3301
Andrew Pryshchepa <vinglfm@gmail.com>
2018-08-24 09:42:37 +0300

    restart processes in seqance

--- process/cluster/cluster.js
+++ process/cluster/cluster.js
@@ -21,6 +21,34 @@ if(cluster.isMaster) {
         });
     };
 
+    cluster.on('exit', (worker, code, signal) => {
+        if(code !== 0 && !worker.exitedAfterDisconnect) {
+            console.log(`Worker ${worker.id} crashed. Starting a new worker...`);
+            cluster.fork();
+        }
+    });
+
+    // process.on('SIGUSR2', () => { //kill -SIGUSR2 master.process.pid
+    //     const workers = Object.values(cluster.workers);
+    //     const restartWorker = (workerIndex) => {
+    //         const worker = workers[workerIndex];
+    //         if(!worker) {
+    //             return;
+    //         }
+    //         worker.on('exit', () => {
+    //             if(!worker.exitedAfterDisconnect) {
+    //                 return;
+    //             }
+    //             console.log(`Exited process ${worker.process.pid}`);
+    //             cluster.fork().on('listening', () => {
+    //                 restartWorker(workerIndex + 1);
+    //             });
+    //         });
+    //         worker.disconnect();
+    //     }
+    //     restartWorker(0);
+    // });//for linux
+
     updateWorkers();
     setInterval(updateWorkers, 15000);
 } else {
