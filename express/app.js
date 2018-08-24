9e5a7a29b4299f727e670faa4626ff04613094c4
Andrew Pryshchepa <vinglfm@gmail.com>
2018-08-24 18:40:10 +0300

    serving dependencies from node_modules

--- express/app.js
+++ express/app.js
@@ -7,6 +7,11 @@ const path = require('path');
 const app = express();
 
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, '..', 'node_modules', 'bootstrap', 'dist', 'css')));
+app.use('/js', express.static(path.join(__dirname, '..', 'node_modules', 'bootstrap', 'dist', 'js')));
+app.use('/js', express.static(path.join(__dirname, '..', 'node_modules', 'jquery', 'dist')));
+app.use('/js', express.static(path.join(__dirname, '..', 'node_modules', 'popper.js', 'dist')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
